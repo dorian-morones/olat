bceef00e31a21a3f022a73a3ac4288139c5c6896
dorian-morones <dorianm393@gmail.com>
2020-08-28 17:03:00 -0700

    General updates

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = '[Your Name]'
-export const siteTitle = 'dmx'
+export const siteTitle = 'olat'
 
 export default function Layout({ children, home }) {
   return (
